47ad39ce27347afbd37f3ff52b1871982d671159
arkk200 <arkk20000@gmail.com>
2022-12-09 23:43:02 +0900

    ADD :: 기본 구조 세팅

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+namespace fs = std::filesystem;
+
+vector<string> split(string sentence, char Separator)
+{
+    vector<string> answer;
+    stringstream ss(sentence);
+    string tmp;
+ 
+    while (getline(ss, tmp, Separator)) {
+        answer.push_back(tmp);
+    }
+    return answer;
+}
+
+void setShortcut(vector<string> command);
+void runCommand(vector<string> command);
+
+int main(){
+    string field = "#";
+    #if __linux__
+        field = "$ ";
+    #elif _WIN32
+        field = ">";
+    #endif
+
+    string stringCommand;
+    vector<string> command;
+    while(true) {
+        cout << fs::current_path().root_path() << field; 
+        getline(cin, stringCommand);
+        command = split(stringCommand, ' ');
+        if(command[0] == "shortcut") {
+            /* 이명재가 구현할 함수 */
+            setShortcut(command);
+        } else {
+            /* 이상진이 구현할 함수 */
+            runCommand(command);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+    return 0;
+}
+
+void setShortcut(vector<string> command){
+
+}
+void runCommand(vector<string> command){
+
+}
